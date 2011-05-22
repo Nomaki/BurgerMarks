1d038028e9275d53131d2e6818dace6e7962317f
Romain <68727@supinfo.com>
2011-05-22 23:09:09 +0200

    Premier BURGER commit

---
+++ BurgerMarks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BurgerMarks::Application.config.secret_token = '6406ca26abbe91450cfa61c2394e240535ad95ca0b34b8910f9b73b6f9554f7e3650a4acdc0629a4109dd3d69efa1cc048938fbdd5fc726c460e9586786e533b'
